5cb43570b5bf3bf06c1305b8b520decae8581bc1
nadirbashir <1.nadirbashir@gmail.com>
2021-04-20 17:26:59 +0500

    task 8 complete

---
+++ app.js
@@ -0,0 +1,29 @@
+// var number=[], boolArray=[], mixedArray=[];
+// var qualification = [];
+
+// number = [5,3,4,7,2,59];
+// boolArray = [false,true]
+// mixedArray = ["Age", 55, true,66,false,]
+
+// qualification = ["SSC","HSC","BCS","BS","BCOM","MS","M. phil."];
+
+// document.write("<h2>Qualifications</h2>");
+// document.write("1) " + qualification[0]+"<br>");
+
+// for(var i = 1; i < qualification.length; i++){
+    
+//     document.write(i+") " + qualification[i]+"<br>");
+// }
+
+
+/*********Question 8 ***********/
+
+var students = ["Nadir", "Ahmed", "Ali"]
+var obtMarks = [320, 230, 480]
+var totalMarks = 500
+
+document.write("Score of "+ students[0] +" is " + obtMarks[0] + ". Percentage: " + (obtMarks[0]/totalMarks) * 100 + "%<br>");
+
+document.write("Score of "+ students[1] +" is " + obtMarks[1] + ". Percentage: " + (obtMarks[1]/totalMarks) * 100 + "%<br>");
+
+document.write("Score of "+ students[2] +" is " + obtMarks[2] + ". Percentage: " + (obtMarks[2]/totalMarks) * 100 + "%<br>");
